4caeecefa2ddc6a4f84c7dd9de36528ca6cf0a65
rootKid <137388933+devMainak@users.noreply.github.com>
2024-07-23 17:52:24 +0000

    feat: remainingItems page

--- src/pages/Remaining.jsx
+++ src/pages/Remaining.jsx
@@ -1,12 +1,29 @@
-import 
+import { useSelector, useDispatch } from 'react-redux'
+  import { fetchInventory, fetchRemovedItems } from '../actions'
+  import { useEffect } from 'react'
 
 const Remaining = () => {
-  
+  const dispatch = useDispatch()
+  const inventory = useSelector((state) => state.inventory)
+  const removedItems = useSelector((state) => state.removedItems)
+
+  useEffect(() => {
+    dispatch(fetchInventory())
+  }, [])
+
+  useEffect(() => {
+    dispatch(fetchRemovedItems())
+  }, [])
+
+  const totalInventoryItems = inventory.reduce((acc, curr) => acc += curr.itemQuantity, 0)
+  const totalRemovedItems = removedItems.reduce((acc, curr) => acc += curr.itemQuantity, 0)
+
+  const totalRemainingItems = totalInventoryItems - totalRemovedItems
   
   return (
     <div>
       <h1>Remaining Items in Inventory</h1>
-      <h2>Inventory Total: </h2>
+      <h2>Inventory Total: {totalRemainingItems}</h2>
     </div>
   )
 }
