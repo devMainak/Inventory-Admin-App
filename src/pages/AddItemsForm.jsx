71fec60c73033d2d9eb70af18671dc9b063d1fc3
rootKid <137388933+devMainak@users.noreply.github.com>
2024-07-23 15:27:07 +0000

    feat: AddItemsForm completed

--- src/pages/AddItemsForm.jsx
+++ src/pages/AddItemsForm.jsx
@@ -1,19 +1,39 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem, removeItem } from '../actions'
+
 const AddItemsForm = () => {
+  const [itemName, setItemName] = useState("")
+  const [itemQuantity, setItemQuantity] = useState(0)
+  const [entryType, setEntryType] = useState("Add to Storage")
+  const dispatch = useDispatch()
+  
+  // function to handle add item form
+  const handleAddItem = (e) => {
+    e.preventDefault()
+    if (entryType === "Add to Storage")
+    {
+      dispatch(addItem({itemName, itemQuantity: parseFloat(itemQuantity), entryType}))
+    } else {
+      dispatch(removeItem({itemName, itemQuantity: parseFloat(itemQuantity), entryType}))
+    }
+  }
+  
   return (
     <div>
       <h1>Inventory Admin App</h1>
-      <form>
+      <form onSubmit={handleAddItem}>
         <label htmlFor="itemName" >Item Name:</label>
         <br/>
-        <input id="itemName" type="text"/>
+        <input onChange={(e) => setItemName(e.target.value)} value={itemName} id="itemName" type="text"/>
         <br/><br/>
         <label htmlFor="itemQuantity" >Item Quantity:</label>
         <br/>
-        <input id="itemQuantity" type="number"/>
+        <input onChange={(e) => setItemQuantity(e.target.value)} value={itemQuantity} id="itemQuantity" type="number"/>
         <br/><br/>
         <label htmlFor="entryType" >Entry Type:</label>
         <br/>
-        <select id="entryType">
+        <select onChange={(e) => setEntryType(e.target.value)} value={entryType} id="entryType">
           <option value="Add to Storage">Add to Storage</option>
           <option value="Remove from Storage">Remove from Storage</option>
         </select>
